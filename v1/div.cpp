a7d72bcb3ef6495c64a52a54340bc0245db4b34c
lwdtksy <lwd2051163668@outlook.com>
2023-12-02 15:48:50 +0800

    all of first commit

---
+++ v1/div.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "head.h"
+
+int div(int a, int b){
+    return a / b;
+}
---
+++ v1/sub.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "head.h"
+
+int sub(int a, int b){
+    return a - b;
+}
